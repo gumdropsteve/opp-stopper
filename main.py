3ac994ed188479014b5fc0256eb1fc5083f8545a
winston <gumdropsteve@winstons-MacBook-Pro.local>
2024-12-25 15:59:18 -0500

    add docs

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-
-
 class Stack:
+    """
+    Load and fire a magazine using LIFO
+
+    Tutorial: https://www.instagram.com/reel/DD0U5opvSDX/
+    """
 
     def __init__(self):
         self.stack = []
 
     def push(self, item):
+        """
+        Push a round into the mag
+        """
         self.stack.append(item)
 
     def pop(self):
+        """
+        Unload a round from the magazine
+        """
         if self.is_empty():
             print("Mag is empty!")
             return None
@@ -17,12 +26,23 @@ class Stack:
             print("Pop!")
         
     def is_empty(self):
+        """
+        Check if the magazine is empty
+
+        Returns True if magazine is empty
+        """
         return len(self.stack) == 0
     
     def size(self):
+        """
+        Print out how many rounds are in the magazine
+        """
         return print("Mag Size: ", len(self.stack))
     
     def display(self):
+        """
+        Print out the magazine
+        """
         if self.is_empty():
             print("Empty Mag")
         else:
